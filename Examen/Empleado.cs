d46f591841f03caaf3d9c925cbb61a7f799415ff
Claros7 <Claros7@users.noreply.github.com>
2019-06-10 13:44:45 -0600

    Commit Final

---
+++ Examen/Empleado.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace Examen
+{
+    public class Empleado : INotifyPropertyChanged
+    {
+        public string Nombre;
+        public string Id;
+        public string HorasTrabajadas;
+        public string 
+    }
+}
